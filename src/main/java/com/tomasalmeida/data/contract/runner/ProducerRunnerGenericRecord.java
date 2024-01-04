016294c19e847d3ab84f68cb6201d64f0ad4825a
Tomás Dias Almeida <tomas.almeida@gmail.com>
2024-01-04 14:56:38 +0100

    fixing jsonata rule

--- src/main/java/com/tomasalmeida/data/contract/runner/ProducerRunnerGenericRecord.java
+++ src/main/java/com/tomasalmeida/data/contract/runner/ProducerRunnerGenericRecord.java
@@ -8,9 +8,9 @@ import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
-import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.errors.SerializationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,15 +18,15 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Properties;
 
-public class ProducerRunner extends Thread {
+public class ProducerRunnerGenericRecord extends Thread {
     private static final String TOPIC_USERS = "crm.users";
     private static final String TOPIC_CONTRACTS = "crm.contracts";
-    private static final Logger LOGGER = LoggerFactory.getLogger(ProducerRunner.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProducerRunnerGenericRecord.class);
 
     private final KafkaProducer<String, User> userProducer;
     private final KafkaProducer<String, Object> contractProducer;
 
-    public ProducerRunner() throws IOException {
+    public ProducerRunnerGenericRecord() throws IOException {
         Properties properties = PropertiesLoader.load("client.properties");
         properties.put(KafkaAvroSerializerConfig.AVRO_USE_LOGICAL_TYPE_CONVERTERS_CONFIG, true);
         properties.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, false);
@@ -38,15 +38,15 @@ public class ProducerRunner extends Thread {
     @Override
     public void run() {
         try {
-            produceUser("Tomas", "Dias Almeida", "Tomas Almeida", 39);
-            produceUser("Fernando", "Perez Machado", "", 53);
-            produceUser("Amon", "Ra", "", 52);
-            produceUser("La", "Fontaine", "", 49);
-            produceUser("Young", "Sheldon Cooper", "", 7);
+//            produceUser("Tomas", "Dias Almeida", "Tomas Almeida", 39);
+//            produceUser("Fernando", "Perez Machado", "", 53);
+//            produceUser("Amon", "Ra", "", 52);
+//            produceUser("La", "Fontaine", "", 49);
+//            produceUser("Young", "Sheldon Cooper", "", 7);
             userProducer.close();
             produceContract(1, "valid contract", LocalDate.of(2030, 12, 31));
-            produceContract(2, "expired contract", LocalDate.of(2021, 12, 31));
-            produceContract(3, "a", LocalDate.of(2122, 12, 31));
+//            produceContract(2, "expired contract", LocalDate.of(2021, 12, 31));
+//            produceContract(3, "a", LocalDate.of(2122, 12, 31));
             contractProducer.close();
         } catch (Exception e) {
             LOGGER.error("Ops", e);
@@ -70,13 +70,46 @@ public class ProducerRunner extends Thread {
     }
 
     private void produceContract(int id, String name, LocalDate expiration) throws InterruptedException {
+        Contract contract = null;
+        String contractSchema = "{\n" +
+                "  \"name\": \"Contract\",\n" +
+                "  \"namespace\": \"com.tomasalmeida.data.contract\",\n" +
+                "  \"type\": \"record\",\n" +
+                "  \"fields\": [\n" +
+                "    {\n" +
+                "      \"name\": \"id\",\n" +
+                "      \"type\": \"int\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"name\",\n" +
+                "      \"type\": \"string\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "      \"name\": \"expiration\",\n" +
+                "      \"type\": {\n" +
+                "        \"type\": \"string\",\n" +
+                "        \"logicalType\": \"date\"\n" +
+                "      }\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+
+        Schema.Parser parser = new Schema.Parser();
+        Schema parsedContractSchema = parser.parse(contractSchema);
+        GenericRecord avroRecord = new GenericData.Record(parsedContractSchema);
+        avroRecord.put("id", id);
+        avroRecord.put("name", name);
+        avroRecord.put("expiration", expiration.toString());
+        ProducerRecord<String, Object> contractRecord = new ProducerRecord<>(TOPIC_CONTRACTS, avroRecord);
+
         try {
-            Contract contract = new Contract (id, name, expiration.toString());
-            LOGGER.info("Sending contract {}", contract);
-            ProducerRecord<String, Object> contractRecord = new ProducerRecord<>(TOPIC_CONTRACTS, contract);
+            contract = new Contract (id, name, expiration.toString());
+            LOGGER.info("Sending contract {}", contractRecord);
+//            ProducerRecord<String, Object> contractRecord = new ProducerRecord<>(TOPIC_CONTRACTS, contract);
             contractProducer.send(contractRecord);
         } catch (SerializationException serializationException) {
             LOGGER.error("Unable to serialize contract: {}", serializationException.getCause().getMessage());
+            LOGGER.error("a", serializationException);
         }
         LOGGER.info("================");
         Thread.sleep(1000);
@@ -84,7 +117,7 @@ public class ProducerRunner extends Thread {
     }
 
     public static void main(final String[] args) throws IOException {
-        ProducerRunner producerRunner = new ProducerRunner();
+        ProducerRunnerGenericRecord producerRunner = new ProducerRunnerGenericRecord();
         producerRunner.run();
     }
 }
